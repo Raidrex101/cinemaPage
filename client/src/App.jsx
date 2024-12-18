ecc613e3695fada522074c7a6896323e4d5d3101
Hector Castro <raidrex@hotmail.com>
2024-12-18 16:39:00 -0600

    installing bootstrap and creating placeholders for login home and register

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,23 @@
 import './App.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from './pages/Home'
+import Login from './pages/login'
+import Register from './pages/Register'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 function App() {
+  const router = createBrowserRouter([
+    { path: '/', element: <Home /> },
+    { path: '/login', element: <Login /> },
+    { path: '/register', element: <Register /> },
+  ]);
+
+
 
   return (
     <>
-      <h1>hola</h1>
+      <RouterProvider router={router} />
     </>
   )
 }
