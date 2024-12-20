dbb6f8ded534451fc136f6815a9403af56b94c64
Hector Castro <raidrex@hotmail.com>
2024-12-19 23:10:47 -0600

    created a schedule with node-cron to make petitions to tmdb each month

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ import userRoutes from './routes/userRoutes.js'
 import movieRoutes from './routes/movieRoutes.js'
 import ticketRoutes from './routes/TicketRoutes.js'
 import authRoutes from './routes/authRoutes.js'
+import cron from 'node-cron'
+import { syncGenresAndMovies } from './config/tmdbSync.js'
 
 const PORT = process.env.PORT || 3000
 
@@ -18,6 +20,16 @@ api.use('/api/v1/movies', movieRoutes)
 api.use('/api/v1/tickets', ticketRoutes)
 api.use('/api/v1', authRoutes)
 api.use('/api/v1', userRoutes)
+
+// configure  syncronization whit TMDB each monday
+cron.schedule('0 0 1 * *', async () => { // cron will schedule tmdb calls to now_playing on the first day of every month at midnight
+  try {
+    await syncGenresAndMovies()
+    console.log('TMDB movies syncronized')
+  } catch (error) {
+    console.error('TMDB movies syncronization error', error)
+  }
+})
 
 connect().then(() => {
   api.listen(PORT, () => {
