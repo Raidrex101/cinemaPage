22c7f1594bce0b540a2f8792e1f9ce0471690e93
Hector Castro <raidrex@hotmail.com>
2024-12-19 23:48:47 -0600

    importing sync cron function

--- server/index.js
+++ server/index.js
@@ -4,14 +4,14 @@ import userRoutes from './routes/userRoutes.js'
 import movieRoutes from './routes/movieRoutes.js'
 import ticketRoutes from './routes/TicketRoutes.js'
 import authRoutes from './routes/authRoutes.js'
-import cron from 'node-cron'
-import { syncGenresAndMovies } from './config/tmdbSync.js'
+import { syncronization } from './config/syncSchedule.js'
 
 const PORT = process.env.PORT || 3000
 
 const api = express()
 
 api.use(express.json())
+syncronization()
 
 // rutas
 
@@ -20,16 +20,6 @@ api.use('/api/v1/movies', movieRoutes)
 api.use('/api/v1/tickets', ticketRoutes)
 api.use('/api/v1', authRoutes)
 api.use('/api/v1', userRoutes)
-
-// configure  syncronization whit TMDB each monday
-cron.schedule('0 0 1 * *', async () => { // cron will schedule tmdb calls to now_playing on the first day of every month at midnight
-  try {
-    await syncGenresAndMovies()
-    console.log('TMDB movies syncronized')
-  } catch (error) {
-    console.error('TMDB movies syncronization error', error)
-  }
-})
 
 connect().then(() => {
   api.listen(PORT, () => {
