cd0b3c4092748cc37f1001c71d5d66e69d0ff619
Hector Castro <raidrex@hotmail.com>
2024-12-20 23:56:07 -0600

    eliminated some fields that now are insite the room model and added a reference to the room taht will play the movie inside movie schema

--- server/models/movieModel.js
+++ server/models/movieModel.js
@@ -1,7 +1,5 @@
 import mongoose from 'mongoose'
 
-const seats = ['A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'A9', 'A10', 'B1', 'B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'B8', 'B9', 'B10', 'C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'C10', 'D1', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7', 'D8', 'D9', 'D10']
-
 const movieSchema = new mongoose.Schema({
   tmdbId: {
     type: Number,
@@ -29,36 +27,24 @@ const movieSchema = new mongoose.Schema({
     type: Number,
     required: true
   },
-  functionTime: {
-    type: Date,
-    required: true
-  },
+  rooms: [{
+    type: mongoose.Schema.Types.ObjectId, ref: 'Room'
+  }],
   rating: {
     type: Number,
     min: 0,
     max: 10,
     required: true
   },
-  seatPrice: {
-    type: Number,
-    required: true
-  },
-  seatsLeft: {
-    type: Number,
-    max: 40,
-    default: 40,
-    required: true
-  },
-  seatsAviable: {
-    type: [String],
-    enum: seats,
-    default: () => seats // al crear el documento se ejecuta la funcion para que utilice los valores por defecto en automatico para cada pelicula creada
-  },
   overview: {
     type: String
   },
   poster: {
     type: String
+  },
+  seatPrice: {
+    type: Number,
+    required: true
   },
   isActive: {
     type: Boolean,
