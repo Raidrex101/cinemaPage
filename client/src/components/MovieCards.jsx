be30615e471512cd18c4ae15bcb2d47d4fe22d4f
Hector Castro <raidrex@hotmail.com>
2024-12-21 18:26:00 -0600

    movie card created for rendering the movie data in home

---
+++ client/src/components/MovieCards.jsx
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom'
+
+const MovieCards = ({id, title, duration, poster}) => {
+    return (
+      <div key={id} className="card border-0 mt-3 cardwidth">
+  <img className="rounded-4 shadow p-0 mb-2 bg-body-tertiary" src={poster} alt={title} />
+    <span className="text-start text text-muted">{duration} min</span>
+  <div className=" text-start fs-6 fw-bold">
+    <p className="card-title">{title}</p>
+    <Link to={`/movie/${id}`}>
+      See details
+    </Link>
+  </div>
+</div>
+
+    )
+  }
+  export default MovieCards
+  
