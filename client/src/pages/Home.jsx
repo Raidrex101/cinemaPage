aa160f9c058bb385ec61293ce4c912f2f1d82985
Hector Castro <raidrex@hotmail.com>
2024-12-21 18:32:33 -0600

    created map for each movie to have its own movie card

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,29 @@
+import { MovieContext } from "../context/movieContext"
+import { useContext } from "react"
+import MovieCards from "../components/MovieCards"
 const Home = () => {
+  const { movies } = useContext(MovieContext)
+  
   return (
-    <div>Home</div>
+    <div className="container-fluid text-center custommt">
+    <section className="d-flex flex-wrap justify-content-evenly pt-auto">
+    {movies.length > 0 ? (
+      movies.map((movie) => (
+        <MovieCards
+          key={movie._id}
+          id={movie._id}
+          title={movie.name}
+          overview={movie.overview}
+          poster={movie.poster}
+          duration={movie.durationMins}
+        />
+      ))
+    ) : (
+      <h2>Loading...</h2>
+    )}
+  </section>
+
+    </div>
   )
 }
 export default Home
