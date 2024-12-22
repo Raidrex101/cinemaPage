c089bf653d6cafdc1aab8afbe40aa65c939ab5d4
Hector Castro <raidrex@hotmail.com>
2024-12-21 18:30:09 -0600

    context to show all movies on the page when needed

---
+++ client/src/context/movieContext.jsx
@@ -0,0 +1,37 @@
+import { createContext, useState, useEffect } from 'react'
+
+const MovieContext = createContext()
+
+const MovieProvider = ({ children }) => {
+    const mainUrl = import.meta.env.VITE_CINEMA_API
+    const [movies, setMovies] = useState([])
+
+    useEffect(() => {
+        const fetchMovies = async () => {
+            try {
+                const response = await fetch(`${mainUrl}/movies`)
+                if (!response.ok) {
+                    throw new Error(`Error fetching movies: ${response.status} ${response.statusText}`)
+                }
+                const data = await response.json()
+                console.log('Movies data:', data)  // Verificar la respuesta de la API
+                setMovies(data)
+            } catch (error) {
+                console.error('Error fetching movies:', error)
+            }
+        }
+        fetchMovies()
+    }, [])
+
+    const data = {
+        movies
+    }
+
+    return (
+        <MovieContext.Provider value={data}>
+            {children}
+        </MovieContext.Provider>
+    )
+}
+
+export { MovieContext, MovieProvider }
