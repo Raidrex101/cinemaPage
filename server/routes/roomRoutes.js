4a54dfb882fbfaf76a22d88598bb5ad9f1768e8e
Hector Castro <raidrex@hotmail.com>
2025-01-17 15:57:27 -0600

    more routes created for the rooms

--- server/routes/roomRoutes.js
+++ server/routes/roomRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express'
-import { createRoom } from '../controller/roomController.js'
+import { createRoom, addFunctionTime, getAllRooms } from '../controller/roomController.js'
 import { isAdmin } from '../middlewares/isAdmin.js'
 import { isAuth } from '../middlewares/isAuth.js'
 
 const roomRoutes = express.Router()
 
 roomRoutes.post('/', isAuth, isAdmin, createRoom)
+roomRoutes.get('/', getAllRooms)
+roomRoutes.patch('/:roomId', isAuth, isAdmin, addFunctionTime)
 
 export default roomRoutes
