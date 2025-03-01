c2e6fa51c160f364da2f1176e577a106c454051f
Hector Castro <raidrex@hotmail.com>
2025-03-01 13:29:06 -0600

    created controllers to manage which room will have a movie to play and which room will be aviable to hace a function

--- server/controller/roomController.js
+++ server/controller/roomController.js
@@ -1,3 +1,4 @@
+import Movie from '../models/movieModel.js'
 import Room from '../models/roomModel.js'
 
 // CREATE
@@ -19,7 +20,11 @@ const createRoom = async (req, res) => {
 const getAllRooms = async (req, res) => {
   try {
     const rooms = await Room
-      .find({ isActive: true })
+      .find({})
+      .populate({
+        path: 'functionTimes.movie',
+        select: 'name poster seatPrice'
+      })
     res.status(200).json(rooms)
   } catch (error) {
     res.status(500).json({ message: 'Error getting rooms', error })
@@ -47,14 +52,51 @@ const addFunctionTime = async (req, res) => {
     })
 
     await room.save()
+
+    const movie = await Movie.findById(movieId)
+    if (!movie) {
+      return res.status(404).json({ message: 'Movie not found' })
+    }
+
+    if (!movie.rooms.includes(roomId)) {
+      movie.rooms.push(roomId)
+      await movie.save()
+    }
+
     res.status(200).json({ message: 'Function time added successfully', room })
   } catch (error) {
     res.status(500).json({ message: 'Error adding function time', error })
   }
 }
 
+const manageIsActive = async (req, res) => {
+  try {
+    const room = await Room.findById(req.body.roomId)
+    if (!room) {
+      return res.status(404).json({ message: 'Room not found' })
+    }
+
+    if (room.isActive) {
+      if (room.seats < 40) {
+        return res.status(400).json({ message: 'Some seats are bought, you cannot deactivate the room' })
+      }
+      room.isActive = false
+      room.functionTimes = []
+    } else {
+      room.isActive = true
+    }
+
+    await room.save()
+    res.status(200).json({ message: 'Room status updated successfully', isActive: room.isActive })
+  } catch (error) {
+    console.error('Error changing room status:', error)
+    res.status(500).json({ message: 'Error updating room status', error })
+  }
+}
+
 export {
   createRoom,
   getAllRooms,
-  addFunctionTime
+  addFunctionTime,
+  manageIsActive
 }
