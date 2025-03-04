d145dfab0ca57a640c1d492fb968341993e12701
Hector Castro <raidrex@hotmail.com>
2025-03-03 20:41:56 -0600

    now each date is a button that will render all the functions of a selected movie

--- client/src/components/TicketDates.jsx
+++ client/src/components/TicketDates.jsx
@@ -1,23 +1,30 @@
-const TicketDates = () => {
-  const today = new Date()
-  const dates = []
+const TicketDates = ({ setSelectedDate, selectedDate }) => {
+  const today = new Date();
+  const dates = [];
 
   for (let i = 0; i < 7; i++) {
-    const currentDate = new Date(today)
-    currentDate.setDate(today.getDate() + i)
+    const currentDate = new Date(today);
+    currentDate.setDate(today.getDate() + i);
+    const formattedDate = currentDate.toISOString().split("T")[0]; // Formato YYYY-MM-DD
+
     dates.push({
-      day: currentDate.toLocaleDateString('en-US', { weekday: 'long' }),
-      date: currentDate.toLocaleDateString('en-US', { month: 'short', day: 'numeric' })
+      day: currentDate.toLocaleDateString("en-US", { weekday: "long" }),
+      date: currentDate.toLocaleDateString("en-US", { month: "short", day: "numeric" }),
+      formattedDate,
     });
   }
 
   return (
-    <div className="d-flex justify-content-around bg-light rounded-3 p-3">
+    <div className="d-flex justify-content-evenly rounded-3 p-3 ">
       {dates.map((date, index) => (
-        <div key={index} className="text-center">
+        <button
+          key={index}
+          className={`btn ${selectedDate === date.formattedDate ? "btn-primary" : "btn-outline-primary"}`}
+          onClick={() => setSelectedDate(date.formattedDate)}
+        >
           <span className="d-block fw-bold text-muted">{date.day}</span>
           <span className="d-block fw-bold fs-5">{date.date}</span>
-        </div>
+        </button>
       ))}
     </div>
   );
