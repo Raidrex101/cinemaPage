f12d995a705c501fb9fe6457e3b49f5eb88931a4
Hector Castro <raidrex@hotmail.com>
2025-03-04 22:37:48 -0600

    added logic to a propper render and filter of scheduled functions depending on the date

--- client/src/pages/Management.jsx
+++ client/src/pages/Management.jsx
@@ -9,43 +9,39 @@ const Management = () => {
   const { autenticated, userPayload } = useAuthContext();
   const { movies } = useContext(MovieContext);
   const mainUrl = import.meta.env.VITE_CINEMA_API;
-  const [rooms, setRooms] = useState([])
+  const [rooms, setRooms] = useState([]);
 
   const getAllRooms = async () => {
     try {
       const response = await fetch(`${mainUrl}/rooms`);
-
       if (!response.ok) {
-        throw new Error(
-          `Error fetching rooms: ${response.status} ${response.statusText}`
-        );
+        throw new Error(`Error fetching rooms: ${response.status} ${response.statusText}`);
       }
-
       const data = await response.json();
       setRooms(data);
     } catch (error) {
       console.error("Error fetching rooms:", error);
     }
-  }
+  };
 
   useEffect(() => {
     getAllRooms();
-  }, [])
-  
+  }, []);
+
   const activeOrInactiveRoom = async (roomId) => {
     if (userPayload.role !== "ADMIN") {
       console.error("Invalid credentials");
+      return;
     }
-
     const token = localStorage.getItem("token");
 
     if (!token) {
       console.error("No token found");
+      return;
     }
 
     try {
       const response = await isActiveControl(roomId, token);
-
       if (response.status === 200) {
         const updatedRooms = rooms.map((room) => {
           if (room._id === roomId) {
@@ -55,7 +51,6 @@ const Management = () => {
               functionTimes: room.isActive ? [] : room.functionTimes,
             };
           }
-
           return room;
         });
         setRooms(updatedRooms);
@@ -66,11 +61,10 @@ const Management = () => {
       console.error("Error changing room status:", error);
     }
   };
-  
 
   return (
     <>
-     {!autenticated || userPayload?.role !== "ADMIN" ? (
+      {!autenticated || userPayload?.role !== "ADMIN" ? (
         <div>You do not have permission to see this page</div>
       ) : (
         <section className="custommt pt-3">
@@ -89,20 +83,29 @@ const Management = () => {
 
             <div className="row mt-2">
               {rooms.map((room) => {
-                const roomMovies = {}
+                const roomMovies = {};
 
                 room.functionTimes.forEach((ft) => {
-                  const movieName = ft.movie.name
+                  const movieName = ft.movie.name;
+                  const functionDate = ft.date
+                  const functionTime = ft.time
+
                   if (!roomMovies[movieName]) {
-                    roomMovies[movieName] = {
+                    roomMovies[movieName] = {};
+                  }
+
+                  if (!roomMovies[movieName][functionDate]) {
+                    roomMovies[movieName][functionDate] = {
                       times: [],
                       seatPrice: ft.movie.seatPrice,
                       occupiedSeats: [],
                     };
                   }
-                  roomMovies[movieName].times.push(ft.time)
+
+                  roomMovies[movieName][functionDate].times.push(functionTime);
+
                   if (Array.isArray(ft.occupiedSeats)) {
-                    roomMovies[movieName].occupiedSeats.push(...ft.occupiedSeats)
+                    roomMovies[movieName][functionDate].occupiedSeats.push(...ft.occupiedSeats);
                   }
                 });
 
@@ -112,21 +115,28 @@ const Management = () => {
                       <div className="card-body overflow-y-scroll" style={{ height: "13rem" }}>
                         <h5 className="card-title">{room.name}</h5>
                         {Object.keys(roomMovies).length > 0 ? (
-                          Object.entries(roomMovies).map(([movieName, details], index) => (
+                          Object.entries(roomMovies).map(([movieName, dates], index) => (
                             <div key={index}>
                               <p className="card-text">
                                 <strong>Movie {index + 1}: </strong> {movieName}
                               </p>
-                              <p className="card-text">
-                                <strong>Times: </strong> {details.times.join(", ")}
-                              </p>
-                              <p className="card-text">
-                                <strong>Price: </strong> {details.seatPrice || "No price assigned"}
-                              </p>
-                              <p className="card-text">
-                                <strong>Occupied seats?: </strong> {details.occupiedSeats.length > 0 ? "Yes" : "No"}
-                              </p>
-                              <hr />
+                              {Object.entries(dates).map(([date, details], idx) => (
+                                <div key={idx}>
+                                  <p className="card-text">
+                                    <strong>Date: </strong> {date}
+                                  </p>
+                                  <p className="card-text">
+                                    <strong>Times: </strong> {details.times.join(", ")}
+                                  </p>
+                                  <p className="card-text">
+                                    <strong>Price: </strong> {details.seatPrice || "No price assigned"}
+                                  </p>
+                                  <p className="card-text">
+                                    <strong>Occupied seats?: </strong> {details.occupiedSeats.length > 0 ? "Yes" : "No"}
+                                  </p>
+                                  <hr />
+                                </div>
+                              ))}
                             </div>
                           ))
                         ) : (
