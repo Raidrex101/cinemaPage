a9aa03771202efd228dbb948832f7bee739c08b5
Hector Castro <raidrex@hotmail.com>
2025-03-05 19:56:55 -0600

    hotfixv2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -30,15 +30,15 @@ function App() {
       path: '/',
       element: <MainLayout />,
       children: [
-        { path: '', element: <Home /> },
-        { path: 'movie/:id', element: <MoviePage /> },
-        { path: 'management', element: <Management /> },
-        { path: 'buy-tickets/:movieId', element: <BuyTickets /> },
+        { path: '/', element: <Home /> },
+        { path: '/movie/:id', element: <MoviePage /> },
+        { path: '/management', element: <Management /> },
+        { path: '/buy-tickets/:movieId', element: <BuyTickets /> },
       ],
     },
     
-    { path: 'login', element: <Login /> },
-    { path: 'register', element: <Register /> },
+    { path: '/login', element: <Login /> },
+    { path: '/register', element: <Register /> },
     
   ]);
 
@@ -48,7 +48,7 @@ function App() {
     <>
       <MovieProvider>
       <AuthProvider>
-      <RouterProvider router={router} fallbackElement={<div>404 Not Found</div>} />
+      <RouterProvider router={router} />
       </AuthProvider>
       </MovieProvider>
     </>
