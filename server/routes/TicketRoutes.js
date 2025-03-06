ce5ea8ca7fc9986dc07ef6b06ca351544bb2c70d
Hector Castro <raidrex@hotmail.com>
2025-03-06 17:40:40 -0600

    changed routes endpoint

--- server/routes/TicketRoutes.js
+++ server/routes/TicketRoutes.js
@@ -1,11 +1,10 @@
 import express from 'express'
 import { createTicket, getMyTickets } from '../controller/ticketController.js'
 import { isAuth } from '../middlewares/isAuth.js'
-import { isCustomer } from '../middlewares/isCustomer.js'
 
 const ticketRoutes = express.Router()
 
-ticketRoutes.post('/:customerId/:roomId/:movieId', isAuth, isCustomer, createTicket)
-ticketRoutes.get('/:customerId', isAuth, isCustomer, getMyTickets)
+ticketRoutes.post('/createTicket', isAuth, createTicket)
+ticketRoutes.get('/my-tickets/:customerId', isAuth, getMyTickets)
 
 export default ticketRoutes
